7a93ffac77a87e04cdd648450c995f7ad130ad5a
sw333zy <sw333zy@gmail.com>
2019-02-27 11:10:13 -0500

    complete exercise

--- src/main/java/com/example/demo/CourseRepository.java
+++ src/main/java/com/example/demo/CourseRepository.java
@@ -2,5 +2,5 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-public class CourseRepository extends CrudRepository<Course, Long> {
+public interface CourseRepository extends CrudRepository<Course, Long> {
 }
